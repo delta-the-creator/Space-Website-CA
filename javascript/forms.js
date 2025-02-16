c4e61fd30beccce625bc022ccd76861e87250d7b
Not-5Delta <janandaraaj@gmail.com>
2025-02-16 23:36:52 +0000

    Enhance contact form validation and improve README links

---
+++ javascript/forms.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+
+if (input.value.trim() === '') {
+    alert('Please fill out the input field!');
+} else {
+    form.submit();
+    }
+});
